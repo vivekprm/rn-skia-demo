2fcc442c7ea0c6f9ae5567b6da9abdb48ac43ef7
Vivek Mishra <v.mishra@f5.com>
2025-02-04 00:16:12 +0530

    advanced drawing using skia and gesture handler

--- App.js
+++ App.js
@@ -1,32 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { Platform, StyleSheet, View, Text } from 'react-native';
-import CanvasCircle from './CanvasCircle';
-import { useState } from 'react';
-import { LoadSkiaWeb, WithSkiaWeb } from "@shopify/react-native-skia/lib/module/web";
+import DrawingCanvas from "./DrawingCanvas";
+import { WithSkiaWeb } from "@shopify/react-native-skia/lib/module/web";
 
 export default function App() {
-  const [isSkiaLoaded, setIsSkiaLoaded] = useState(false);
-  if (Platform.OS === 'web') {
-    LoadSkiaWeb({locatefile: () => '/canvaskit.wasm'})
-    .then(() => {
-      setIsSkiaLoaded(true);
-    });
-    if (isSkiaLoaded) {
-      return (
-      <WithSkiaWeb
-        getComponent={() => import("./CanvasCircle")}
-      />)
-    } else {
-      return (
-        <View style={styles.container}>
-          <Text>Loading Skia...</Text>
-        </View>
-      )
-    }
+  if (Platform.OS === "web") {
+    return <WithSkiaWeb getComponent={() => import("./DrawingCanvas")} />;
   }
   return (
     <View style={styles.container}>
-      <CanvasCircle />
+      <DrawingCanvas />
       <StatusBar style="auto" />
     </View>
   );
